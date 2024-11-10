2ef7b521191beb4fd9ce53e41235d5c9c51c7b19
AravindKumar40m <aravindkumar40m@gmail.com>
2024-11-10 18:07:14 +0530

    user module created

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const AppError = require("./src/utils/appError");
 const globalErrorHandler = require("./src/controllers/errorController");
 const ProductRouter = require("./src/routers/ProductRoutes");
 const CategoryRouter = require("./src/routers/CategoryRoutes");
-
+const userRouter = require("./src/routers/userRoutes");
 
 const app = express();
 app.use(cookieParser());
@@ -14,23 +14,22 @@ app.use(express.json());
 
 const allowedOrigins = ["http://localhost:5173"];
 app.use(
-    cors({
-        origin: allowedOrigins,
-        credentials: true,
-    })
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
 );
 
+app.use("/api/v1/user", userRouter);
 app.use("/api/v1/product", ProductRouter);
 app.use("/api/v1/categories", CategoryRouter);
 
-
-
 app.get("/", (req, res) => {
-    res.send("API is running...");
+  res.send("API is running...");
 });
 
 app.all("*", (req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
